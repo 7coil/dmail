4b9a3d3f9083250cb434cbfa5d48dc7dbece290e
lepon01 <leondrolio@gmail.com>
2017-07-14 19:10:49 +0100

    check for admins inside the discord ting

--- server/discord/utils/isadmin/index.js
+++ server/discord/utils/isadmin/index.js
@@ -1,6 +1,6 @@
 const config = require('config');
 
 const isadmin = member =>
-	member.permission.has('kickMembers') || member.permission.has('banMembers') || member.permission.has('administrator') || config.get('admins').includes(member.id);
+	member.permission.has('kickMembers') || member.permission.has('banMembers') || member.permission.has('administrator') || config.get('discord').admins.includes(member.id);
 
 module.exports = isadmin;
