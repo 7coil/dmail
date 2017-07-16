ba8009e802bbe68b1d70a8da66f793b963f3be3f
lepon01 <leondrolio@gmail.com>
2017-07-16 22:51:59 +0100

    result[0]

--- server/index.js
+++ server/index.js
@@ -83,88 +83,71 @@ app.get('/', (req, res) => {
 				} else {
 					cursor.toArray((err2, result) => {
 						const cont = (channel) => {
-							if (result.block && Array.isArray(result.block) && result.block.some(email => body.sender.toLowerCase().includes(email))) {
-								res.status(406).send({ success: { message: 'Sender is blocked by recipient' } });
-								console.log('The E-Mail was blocked by the recipient.');
-							} else if (body.subject.length > 128) {
-								console.log('The subject was too long');
-								res.status(406).send({ error: { message: 'The subject was too long' } });
-								sendError(body, 'The subject of your E-Mail was too long to be sent to Discord.');
-							} else if (body.from > 128) {
-								console.log('The author name was too long');
-								res.status(406).send({ error: { message: 'The author name was too long' } });
-								sendError(body, 'Your author name was too long to be sent to Discord.');
-							} else if (banne.some(word => body['body-plain'].toLowerCase().includes(word))) {
-								console.log('The email was detected as spam.');
-								res.status(406).send({ error: { message: 'The email was detected as spam.' } });
-								sendError(body, 'Your email was marked as spam. If you think this was a mistake, or have any other questions, send them to admin@moustacheminer.com');
-							} else {
-								const db = {
-									to,
-									from: body['Reply-To'] || body.From || body.sender,
-									subject: `Re: ${body.Subject}`,
-									reply: body['Message-Id'],
-									reference: body.References ? `${body.References} ${body['Message-Id']}` : body['Message-Id']
-								};
+							const db = {
+								to,
+								from: body['Reply-To'] || body.From || body.sender,
+								subject: `Re: ${body.Subject}`,
+								reply: body['Message-Id'],
+								reference: body.References ? `${body.References} ${body['Message-Id']}` : body['Message-Id']
+							};
 
-								const dm = (plaintext) => {
-									r.table('replies')
-										.insert(db)
-										.run(r.conn, (err, res1) => {
-											if (err) {
-												res.status(406).send({ error: { message: 'An error occured while inserting details into the RethonkDB database.' } });
-												sendError(body, 'An error occured while inserting details into the RethonkDB database. Sorry for the inconvenience.');
-											} else {
-												const content = {
-													embed: {
-														title: body.subject || 'Untitled E-Mail',
-														description: plaintext || 'Empty E-Mail',
-														timestamp: new Date(body.timestamp * 1000),
-														author: {
-															name: body.from || 'No Author'
-														},
-														fields: [
-															{
-																name: 'Reply ID',
-																value: res1.generated_keys[0]
-															}
-														]
-													}
-												};
-
-												if (req.file && req.file.buffer.length < 8000000) {
-													content.file = req.file;
-													channel.createMessage(content, {
-														file: req.file.buffer,
-														name: req.file.originalname || 'unnamed_file'
-													})
-														.then(() => {
-															res.status(200).send({ success: { message: 'Successfully sent message to user.' } });
-														}).catch(() => {
-															res.status(406).send({ error: { message: 'Could not send mail to user.' } });
-															sendError(body, 'The mail server could not DM the user.');
-														});
-												} else {
-													channel.createMessage(content)
-														.then(() => {
-															res.status(200).send({ success: { message: 'Successfully sent message to user.' } });
-														}).catch(() => {
-															res.status(406).send({ error: { message: 'Could not send mail to user.' } });
-															sendError(body, 'The mail server could not DM the user.');
-														});
+							const dm = (plaintext) => {
+								r.table('replies')
+									.insert(db)
+									.run(r.conn, (err, res1) => {
+										if (err) {
+											res.status(406).send({ error: { message: 'An error occured while inserting details into the RethonkDB database.' } });
+											sendError(body, 'An error occured while inserting details into the RethonkDB database. Sorry for the inconvenience.');
+										} else {
+											const content = {
+												embed: {
+													title: body.subject || 'Untitled E-Mail',
+													description: plaintext || 'Empty E-Mail',
+													timestamp: new Date(body.timestamp * 1000),
+													author: {
+														name: body.from || 'No Author'
+													},
+													fields: [
+														{
+															name: 'Reply ID',
+															value: res1.generated_keys[0]
+														}
+													]
 												}
-											}
-										});
-								};
+											};
 
-								if (body['body-plain'].length > 2000) {
-									sendError(body, 'The E-Mail was sent successfully, but DiscordMail recommends less than 2000 characters per message for messages to be displayed correctly within Discord.');
-									gist(body['body-plain'], (url) => {
-										dm(`[The E-Mail was too long to be displayed in Discord.](${url})`);
+											if (req.file && req.file.buffer.length < 8000000) {
+												content.file = req.file;
+												channel.createMessage(content, {
+													file: req.file.buffer,
+													name: req.file.originalname || 'unnamed_file'
+												})
+													.then(() => {
+														res.status(200).send({ success: { message: 'Successfully sent message to user.' } });
+													}).catch(() => {
+														res.status(406).send({ error: { message: 'Could not send mail to user.' } });
+														sendError(body, 'The mail server could not DM the user.');
+													});
+											} else {
+												channel.createMessage(content)
+													.then(() => {
+														res.status(200).send({ success: { message: 'Successfully sent message to user.' } });
+													}).catch(() => {
+														res.status(406).send({ error: { message: 'Could not send mail to user.' } });
+														sendError(body, 'The mail server could not DM the user.');
+													});
+											}
+										}
 									});
-								} else {
-									dm(body['body-plain']);
-								}
+							};
+
+							if (body['body-plain'].length > 2000) {
+								sendError(body, 'The E-Mail was sent successfully, but DiscordMail recommends less than 2000 characters per message for messages to be displayed correctly within Discord.');
+								gist(body['body-plain'], (url) => {
+									dm(`[The E-Mail was too long to be displayed in Discord.](${url})`);
+								});
+							} else {
+								dm(body['body-plain']);
 							}
 						};
 
@@ -174,6 +157,21 @@ app.get('/', (req, res) => {
 						} else if (!result[0]) {
 							res.status(406).send({ error: { message: 'Invalid user - Not found in database.' } });
 							sendError(body, 'The email address does not exist.');
+						} else if (result[0].block && Array.isArray(result[0].block) && result[0].block.some(email => body.sender.toLowerCase().includes(email))) {
+							res.status(406).send({ success: { message: 'Sender is blocked by recipient' } });
+							console.log('The E-Mail was blocked by the recipient.');
+						} else if (body.subject.length > 128) {
+							console.log('The subject was too long');
+							res.status(406).send({ error: { message: 'The subject was too long' } });
+							sendError(body, 'The subject of your E-Mail was too long to be sent to Discord.');
+						} else if (body.from > 128) {
+							console.log('The author name was too long');
+							res.status(406).send({ error: { message: 'The author name was too long' } });
+							sendError(body, 'Your author name was too long to be sent to Discord.');
+						} else if (banne.some(word => body['body-plain'].toLowerCase().includes(word))) {
+							console.log('The email was detected as spam.');
+							res.status(406).send({ error: { message: 'The email was detected as spam.' } });
+							sendError(body, 'Your email was marked as spam. If you think this was a mistake, or have any other questions, send them to admin@moustacheminer.com');
 						} else if (result[0].type === 'user') {
 							discord.getDMChannel(result[0].id)
 								.then((channel) => {
