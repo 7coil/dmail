a69cab8ecb0e064576ab1cf38f20079be64153ab
lepon01 <leondrolio@gmail.com>
2017-07-16 22:42:46 +0100

    log block list

--- server/index.js
+++ server/index.js
@@ -83,6 +83,7 @@ app.get('/', (req, res) => {
 				} else {
 					cursor.toArray((err2, result) => {
 						const cont = (channel) => {
+							console.dir(result.block);
 							if (result.block && Array.isArray(result.block) && result.block.some(email => body.sender.toLowerCase().includes(email))) {
 								res.status(406).send({ success: { message: 'Sender is blocked by recipient' } });
 								console.log('The E-Mail was blocked by the recipient.');
