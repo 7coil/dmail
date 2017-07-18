17b5c7f240b6202915803553383519e5699e2f32
lepon01 <leondrolio@gmail.com>
2017-07-18 01:17:17 +0100

    Improve database for collection

--- server/discord/utils/collection/index.js
+++ server/discord/utils/collection/index.js
@@ -1,54 +1,90 @@
 const r = require('./../../../db');
 const client = require('./../../');
+const config = require('config');
 
 function guildStats(guild) {
-	const name = guild.name;
-	const id = guild.id;
+	// Guild stats!
 	const bots = guild.members.filter(member => member.user.bot).length;
-	const total = guild.members.size;
-	const users = total - bots;
-	const percentage = Math.floor((bots / total) * 100);
-	const fail = percentage > 50 && total > 20;
-	const pass = !fail;
+	const users = guild.memberCount - bots;
+	const percentage = Math.floor((bots / guild.memberCount) * 100);
+	const timestamp = Date.now();
+	const collection = percentage > config.get('collection').percentage && guild.memberCount > config.get('collection').users;
 
-	return { name, id, users, bots, total, percentage, pass, fail };
+	// Owner!
+	const owner = client.users.get(guild.ownerID);
+
+	return {
+		guildID: guild.id,
+		ownerID: owner.id,
+		owner: {
+			id: guild.ownerID,
+			avatar: owner.dynamicAvatarURL('webp', 2048),
+			name: owner.username,
+			discriminator: owner.discriminator,
+			bot: owner.bot,
+			createdAt: owner.createdAt,
+		},
+		guild: {
+			id: guild.id,
+			name: guild.name,
+			createdAt: guild.createdAt,
+			icon: guild.iconURL,
+			members: guild.memberCount,
+			region: guild.region,
+			users,
+			bots,
+			collection,
+		},
+		timestamp
+	};
 }
 
-function banGuild(id) {
-	client.guilds.get(id).leave();
+function banGuild(id, reason) {
+	const guild = client.guilds.get(id);
+	const stats = guildStats(guild);
+
+	stats.reason = reason;
 	r.table('collection')
-		.insert({
-			guild: id
-		})
+		.insert(guildStats(guild))
 		.run(r.conn, (err) => {
 			if (err) throw new Error('Couldn\'t save banned guild.');
 			console.log(`Banned ${client.guilds.get(id).name}`);
+			guild.leave();
 		});
 }
 
 function checkGuilds(bot) {
-	bot.guilds.filter(guild => guildStats(guild).fail).forEach(guild => banGuild(guild.id));
+	bot.guilds.filter(guild => guildStats(guild).guild.collection).forEach(guild => banGuild(guild.id, {
+		message: 'Bot collection guild',
+		maxUsers: config.get('collection').users,
+		maxPercentage: config.get('collection').percentage
+	}));
 }
 
 client.on('guildCreate', (guild) => {
 	const report = guildStats(guild);
 	console.log(report);
 
-	if (report.fail) {
+	if (report.guild.collection) {
 		console.log(`${guild.name} failed the authentication test`);
 		banGuild(guild.id);
 	} else {
 		r.table('collection')
-			.filter({ guild: guild.id })
+			.filter(
+				r.row('guildID').eq(guild.id).or(r.row('ownerID').eq(guild.ownerID))
+			)
 			.run(r.conn, (err1, cursor) => {
 				if (err1) throw new Error('Failed to search banned guild database.');
 				cursor.toArray((err2, result) => {
 					if (err2) throw new Error('Failed to convert banned cursor to results.');
-					if (result.length === 0) {
+					if (result.length < 1) {
 						console.log(`${guild.name} passed the authentication test`);
 					} else {
 						console.log(`${guild.name} was already banned!`);
-						guild.leave();
+						banGuild(guild.id, {
+							message: 'Already banned',
+							refer: result.map(reason => reason.id) || []
+						});
 					}
 				});
 			});
