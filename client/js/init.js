ba9bff8514ea6e625b584866911b795a26248444
lepon01 <leondrolio@gmail.com>
2017-07-26 00:04:50 +0100

    resize on page load

--- client/js/init.js
+++ client/js/init.js
@@ -1,14 +1,17 @@
 /* eslint-env browser */
 /* globals $ Materialize */
 
+const onresize = () => {
+	$('#wheeloffortune').height($(window).height() - $('nav').height() - $('footer').height() - 20);
+};
+
 $(document).ready(() => {
 	$('.carousel.carousel-slider').carousel({
 		fullWidth: true,
 		indicators: false
 	});
 	Materialize.showStaggeredList('#staggered-text');
+	onresize();
 });
 
-$(window).resize(() => {
-	$('#wheeloffortune').height($(window).height() - $('nav').height() - $('footer').height() - 20);
-}).resize();
+$(window).resize(onresize).resize();
