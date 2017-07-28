60f4549981ff3ca6f083e586917f74fd0b2e5c1e
lepon01 <leondrolio@gmail.com>
2017-07-28 15:47:49 +0100

    Remove promotional things

--- client/js/db.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('promo').innerHTML = `<iframe src="https://bots.discord.pw/bots/330003632298917889"></iframe>`;
--- client/js/dbl.js
+++
@@ -1,9 +0,0 @@
-const dblupvote = (positive) => {
-	const type = positive ? 'upvote' : 'none';
-	$.post('https://discordbots.org/api/vote', { bot: '330003632298917889', type }, (data, status, xhr) => {
-		console.log(data);
-		console.log(xhr.status);
-	}, 'json');
-};
-
-document.getElementById('promo').innerHTML = doc;
