4bbcc9f3d7a4ab3affd94ec5f2fe73e0547551cd
lepon01 <leondrolio@gmail.com>
2017-08-12 21:25:38 +0100

    Move timestamp info info command

--- server/discord/cogs/info/index.js
+++ server/discord/cogs/info/index.js
@@ -1,5 +1,4 @@
 const os = require('os');
-const utils = require('./../../utils.js');
 const client = require('./../../');
 const config = require('config');
 
@@ -7,6 +6,49 @@ const config = require('config');
 // (x64) AMD Athlon (x3) Something @ 3ghz (clocked at ????MHz)
 const hardwareinfo = `(${os.arch()}) ${os.cpus()[0].model} clocked at ${os.cpus()[0].speed} MHz`;
 const softwareinfo = `[${os.type()}] ${os.release()}`;
+
+const timestamp = (input) => {
+	let d = 0;
+	let h = 0;
+	let m = 0;
+	let s = 0;
+
+	m = Math.floor(input / 60);
+	s = Math.floor(input % 60);
+
+	h = Math.floor(m / 60);
+	m %= 60;
+
+	d = Math.floor(h / 24);
+	h %= 24;
+
+	let message = '';
+	if (d === 1) {
+		message += `${d} day `;
+	} else if (d > 1) {
+		message += `${d} days `;
+	}
+
+	if (h === 1) {
+		message += `${h} hour `;
+	} else if (h > 1) {
+		message += `${h} hours `;
+	}
+
+	if (m === 1) {
+		message += `${m} minute `;
+	} else if (m > 1) {
+		message += `${m} minutes `;
+	}
+
+	if (s === 1) {
+		message += `${s} second `;
+	} else if (s > 1) {
+		message += `${s} seconds `;
+	}
+
+	return message || 'Literally no time whatsoever';
+};
 
 module.exports.info = {
 	name: 'Info about DiscordMail',
@@ -27,7 +69,7 @@ module.exports.command = (message) => {
 				},
 				{
 					name: 'Uptime',
-					value: utils.timestamp(process.uptime()),
+					value: timestamp(process.uptime()),
 					embed: true
 				},
 				{
@@ -50,7 +92,7 @@ module.exports.command = (message) => {
 				},
 				{
 					name: 'Licence',
-					value: `This copy of [Discord Mail](https://discordmail.com/) is licenced under the MIT Licence. View the [GitHub repository here](https://${config.get('api').mailgun.domain}/github) or [our guild](https://${config.get('api').mailgun.domain}/help) for more information.`
+					value: `This copy of [Discord Mail](https://discordmail.com/) is licenced under the MIT Licence. View the [GitHub repository here](https://${config.get('api').mailgun.domain}/url/github) or [our guild](https://${config.get('api').mailgun.domain}/url/help) for more information.`
 				}
 			]
 		}
