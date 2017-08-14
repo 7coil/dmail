e68e603187486605ecd382a9442213543549165b
lepon01 <leondrolio@gmail.com>
2017-08-14 22:45:56 +0100

    Fix invite link

--- server/url.js
+++ server/url.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const config = require('config');
+const discord = require('./discord');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
 	res.redirect('/');
 })
+	.use('/invite', (req, res) => {
+		res.redirect(`https://discordapp.com/oauth2/authorize?=&client_id=${discord.user.id}&scope=bot&permissions=0`);
+	})
 	.get('/:page', (req, res, next) => {
 		if (config.get('url')[req.params.page]) {
 			res.redirect(config.get('url')[req.params.page]);
