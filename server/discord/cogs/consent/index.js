755337be806c02d664df621316beebd45ae50c9b
lepon01 <leondrolio@gmail.com>
2017-08-16 00:05:35 +0100

    Add consent command to agree to TOS

---
+++ server/discord/cogs/consent/index.js
@@ -0,0 +1,45 @@
+const r = require('./../../../db');
+const config = require('config');
+
+module.exports.info = {
+	name: 'Agree to Terms and Conditions',
+	category: 'mail',
+	aliases: [
+		'agree',
+		'consent',
+		'takemyfuckinglifeaway'
+	]
+};
+
+module.exports.command = (message) => {
+	if (message.context === 'user') {
+		r.table('registrations')
+			.insert({
+				id: message.inbox,
+				type: 'user',
+				details: {
+					name: message.name,
+					discrim: message.author.discriminator
+				},
+				display: `${message.author.username}#${message.author.discriminator}`,
+				email: `${message.name}#${message.author.discriminator}`,
+				block: []
+			}, {
+				conflict: 'update'
+			})
+			.run(r.conn, (err, res) => {
+				if (err) {
+					message.channel.createMessage('An error occured writing your registration to the database.');
+				} else {
+					message.channel.createMessage({
+						embed: {
+							title: `Welcome to ${config.get('name')}!`,
+							description: `${res.replaced ? 'Reassigned' : 'Assigned'} \`${message.name}#${message.author.discriminator}@${config.get('api').mailgun.domain}\` to your account.\nPlease consult the [DiscordMail Terms of Service](https://${config.get('api').mailgun.domain}/docs/terms) and [Privacy Agreement](https://${config.get('api').mailgun.domain}/docs/privacy) before continuing.`
+						}
+					});
+				}
+			});
+	} else {
+		message.channel.createMessage(`To register the guild, please fill this form in. https://${config.get('api').mailgun.domain}/url/guild`);
+	}
+};
--- server/discord/cogs/register/index.js
+++ server/discord/cogs/register/index.js
@@ -11,34 +11,10 @@ module.exports.info = {
 };
 
 module.exports.command = (message) => {
-	if (message.context === 'user') {
-		r.table('registrations')
-			.insert({
-				id: message.inbox,
-				type: 'user',
-				details: {
-					name: message.name,
-					discrim: message.author.discriminator
-				},
-				display: `${message.author.username}#${message.author.discriminator}`,
-				email: `${message.name}#${message.author.discriminator}`,
-				block: []
-			}, {
-				conflict: 'update'
-			})
-			.run(r.conn, (err, res) => {
-				if (err) {
-					message.channel.createMessage('An error occured writing your registration to the database.');
-				} else {
-					message.channel.createMessage({
-						embed: {
-							title: `Welcome to ${config.get('name')}!`,
-							description: `${res.replaced ? 'Reassigned' : 'Assigned'} \`${message.name}#${message.author.discriminator}@${config.get('api').mailgun.domain}\` to your account.\nPlease consult the [DiscordMail Terms of Service](https://${config.get('api').mailgun.domain}/docs/terms) and [Privacy Agreement](https://${config.get('api').mailgun.domain}/docs/privacy) before continuing.`
-						}
-					});
-				}
-			});
-	} else {
-		message.channel.createMessage(`To register the guild, please fill this form in. https://${config.get('api').mailgun.domain}/url/guild`);
-	}
+	message.channel.createMessage({
+		embed: {
+			title: `Welcome to ${config.get('name')}!`,
+			description: `Your account has not been created yet. Please read the [DiscordMail Terms of Service](https://${config.get('api').mailgun.domain}/docs/terms) and [Privacy Agreement](https://${config.get('api').mailgun.domain}/docs/privacy) before continuing. If you agree, please run the \`dmail agree\` command to complete your registration`
+		}
+	});
 };
