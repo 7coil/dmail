ee885006efbe4e9201a4cef9fbcaf9953e125854
lepon01 <leondrolio@gmail.com>
2017-08-19 18:59:20 +0100

    Use name in config

--- server/discord/cogs/register/index.js
+++ server/discord/cogs/register/index.js
@@ -13,7 +13,7 @@ module.exports.command = (message) => {
 	message.channel.createMessage({
 		embed: {
 			title: `Welcome to ${config.get('name')}!`,
-			description: `Your account has not been created yet. Please read the [DiscordMail Terms of Service](${config.get('webserver').domain}/docs/terms) and [Privacy Agreement](${config.get('webserver').domain}/docs/privacy) before continuing. If you agree, please run the \`dmail agree\` command to complete your registration`
+			description: `Your account has not been created yet. Please read the [${config.get('name')} Terms of Service](${config.get('webserver').domain}/docs/terms) and [Privacy Agreement](${config.get('webserver').domain}/docs/privacy) before continuing. If you agree, please run the \`dmail agree\` command to complete your registration`
 		}
 	});
 };
