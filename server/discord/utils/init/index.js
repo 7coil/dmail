9a3eafa3fd2893b6b0e17103e48769e5da4260d5
lepon01 <leondrolio@gmail.com>
2017-09-18 17:14:12 +0100

    Add message.words

--- server/discord/utils/init/index.js
+++ server/discord/utils/init/index.js
@@ -8,6 +8,7 @@ const init = (message, pre, next) => {
 	message.prefix = pre[1];
 	message.command = pre[2];
 	message.input = pre[3] || null;
+	message.words = (pre[3] || '').split(' ');
 	message.name = utils.dmail.name(message.author.username);
 	message.context = config.get('discord').prefix.user.includes(message.prefix.toLowerCase()) ? 'user' : 'guild';
 	message.inbox = message.context === 'user' ? message.author.id : (message.channel.guild && message.channel.guild.id) || 'Not inside a guild';
