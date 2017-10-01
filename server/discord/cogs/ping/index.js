07279ef62b8c745f7d6d5e57f6166cd6125ee1c1
lepon01 <leondrolio@gmail.com>
2017-10-01 01:06:26 +0100

    Add an arrow next to what shard the bot is on

--- server/discord/cogs/ping/index.js
+++ server/discord/cogs/ping/index.js
@@ -7,5 +7,12 @@ module.exports.info = {
 	]
 };
 
-module.exports.command = message =>
-	message.channel.createMessage(`\`\`\`\n${client.shards.map(shard => `Shard ${shard.id} | ${shard.latency}ms`).join('\n')}\n\`\`\``);
+module.exports.command = (message) => {
+	let s = 0;
+
+	if (message.channel.guild) {
+		s = client.guildShardMap[message.channel.guild.id];
+	}
+
+	message.channel.createMessage(`\`\`\`\n${client.shards.map(shard => `${s === shard.id ? '>' : ' '}Shard ${shard.id} | ${shard.latency}ms`).join('\n')}\n\`\`\``);
+};
