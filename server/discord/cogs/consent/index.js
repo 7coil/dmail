db0c171a4bbc5e7d6a978590cb4a8438451f147e
lepon01 <leondrolio@gmail.com>
2017-10-02 18:04:17 +0100

    Prevent re-registrations and log errors when it can't DM

--- server/discord/cogs/consent/index.js
+++ server/discord/cogs/consent/index.js
@@ -1,6 +1,7 @@
-const r = require('./../../../db');
+const r = require('../../../db');
 const config = require('config');
 const mailgun = require('mailgun-js')(config.get('api').mailgun);
+const client = require('../../');
 
 module.exports.info = {
 	aliases: [
@@ -8,46 +9,62 @@ module.exports.info = {
 		'consent',
 		'takemyfuckinglifeaway'
 	],
-	ratelimit: 30000
+	ratelimit: 500
 };
 
 module.exports.command = (message) => {
 	if (message.context === 'user') {
-		r.table('registrations')
-			.insert({
-				id: message.inbox,
-				type: 'user',
-				details: {
-					name: message.name,
-					discrim: message.author.discriminator
-				},
-				display: `${message.author.username}#${message.author.discriminator}`,
-				email: `${message.name}#${message.author.discriminator}`,
-				block: []
-			}, {
-				conflict: 'update'
-			})
-			.run(r.conn, (err) => {
-				if (err) {
-					message.channel.createMessage(message.__('err_generic'));
-				} else {
-					const data = {
-						from: `${config.get('name')} Mail Server <noreply@${config.get('api').mailgun.domain}>`,
-						to: `${message.name}#${message.author.discriminator}@${config.get('api').mailgun.domain}`,
-						subject: message.__('consent_subject', { name: message.__('name') }),
-						html: config.get('welcome')
-					};
-
-					mailgun.messages().send(data, (err2) => {
-						if (err2) {
+		client.getDMChannel(message.inbox)
+			.then((channel) => {
+				r.table('registrations')
+					.insert({
+						id: message.inbox,
+						type: 'user',
+						details: {
+							name: message.name,
+							discrim: message.author.discriminator
+						},
+						display: `${message.author.username}#${message.author.discriminator}`,
+						email: `${message.name}#${message.author.discriminator}`,
+						block: []
+					})
+					.run(r.conn, (err, res) => {
+						if (err) {
 							message.channel.createMessage(message.__('err_generic'));
-							console.log(`Failed to send an introductory email to ${message.name}#${message.author.discriminator}@${config.get('api').mailgun.domain}`);
+						} else if (res.errors) {
+							message.channel.createMessage(message.__('err_registered'));
 						} else {
-							message.channel.createMessage(message.__('consent_message'));
-							console.log((new Date()).toUTCString(), `Sent introductory email to ${message.name}#${message.author.discriminator}@${config.get('api').mailgun.domain}`);
+							const data = {
+								from: `${config.get('name')} Mail Server <noreply@${config.get('api').mailgun.domain}>`,
+								to: `${message.name}#${message.author.discriminator}@${config.get('api').mailgun.domain}`,
+								subject: message.__('consent_subject', { name: message.__('name') }),
+								html: config.get('welcome')
+							};
+
+							channel.createMessage(`${message.name}#${message.author.discriminator}@${config.get('api').mailgun.domain}`)
+								.then(() => {
+									message.channel.createMessage(message.__('consent_message'));
+									mailgun.messages().send(data, (err2) => {
+										if (err2) {
+											message.channel.createMessage(message.__('err_generic'));
+											console.log(`Failed to send an introductory email to ${message.name}#${message.author.discriminator}@${config.get('api').mailgun.domain}`);
+										} else {
+											console.log((new Date()).toUTCString(), `Sent introductory email to ${message.name}#${message.author.discriminator}@${config.get('api').mailgun.domain}`);
+										}
+									});
+								})
+								.catch(() => {
+									message.channel.createMessage(message.__('err_dm'));
+									r.table('registrations')
+										.get(message.inbox)
+										.delete()
+										.run(r.conn);
+								});
 						}
 					});
-				}
+			})
+			.catch(() => {
+				message.channel.createMessage(message.__('err_dm'));
 			});
 	} else {
 		message.channel.createMessage(message.__('consent_guild', { url: `${config.get('webserver').domain}/url/guild` }));
