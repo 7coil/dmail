c3b2c6ed23cd15881e8c9dc1611a42d1ac570846
7coil <leondrolio@gmail.com>
2017-12-22 14:19:11 +0000

    console dir e k thanks

--- server/mail.js
+++ server/mail.js
@@ -168,7 +168,7 @@ router.get('/', authed, registered, async (req, res) => {
 				if (e.resonse && e.response.includes('50007')) {
 					error = res.__('err_dm');
 				} else {
-					error = JSON.stringify(e);
+					console.dir(e);
 				}
 				res.status(500).render('error.pug', {
 					status: 500,
