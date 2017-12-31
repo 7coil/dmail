57a107490e73fc669564ae2c8847692a0d196728
7coil <leondrolio@gmail.com>
2017-12-31 17:55:23 +0000

    Readd upvote script

--- server/static/js/discordbots.org.js
+++ server/static/js/discordbots.org.js
@@ -367,4 +367,22 @@ const dblupvote = (id, elem) => {
 	});
 };
 
+const request = (opts, callback) => {
+	var xhr = new XMLHttpRequest()
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState != 4) return
+		if (xhr.status >= 200 && xhr.status < 400) {
+			return callback(null, xhr.getResponseHeader("Content-Type") != null && xhr.getResponseHeader("Content-Type").indexOf("application/json") != -1 ? JSON.parse(xhr.responseText) : xhr.responseText, xhr)
+		}
+		var msg = xhr.responseText || "Error"
+		callback(Error(msg), null, xhr)
+	}
+	xhr.open(opts.method || "GET", opts.url)
+	for (var key in opts.headers) {
+		xhr.setRequestHeader(key, opts.headers[key])
+	}
+	xhr.send(opts.data)
+	return xhr
+};
+
 document.addEventListener('DOMContentLoaded', load);
