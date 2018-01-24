410f736bc602fbc6a6380c43d03952e1bdffd4ad
7coil <leondrolio@gmail.com>
2018-01-24 22:28:59 +0000

    Use config options

--- server/mail.js
+++ server/mail.js
@@ -1,9 +1,9 @@
 const SMTPServer = require('smtp-server');
 const fs = require('fs');
+const config = require('config');
 
 const server = new SMTPServer({
-	secure: true,
-	key: fs.readFileSync('private.key'),
-	cert: fs.readFileSync('server.crt')
+	key: fs.readFileSync(config.get('certificate').key),
+	cert: fs.readFileSync(config.get('certificate').cert)
 });
 server.listen(465);
