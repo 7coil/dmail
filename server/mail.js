fd8e7499778405d98abd24071186571aefd80692
7coil <leondrolio@gmail.com>
2018-01-24 22:37:26 +0000

    Log incoming data

--- server/mail.js
+++ server/mail.js
@@ -4,6 +4,10 @@ const config = require('config');
 
 const server = new SMTPServer({
 	key: fs.readFileSync(config.get('certificate').key),
-	cert: fs.readFileSync(config.get('certificate').cert)
+	cert: fs.readFileSync(config.get('certificate').cert),
+	onData(stream, session, callback) {
+		stream.pipe(process.stdout);
+		stream.on('end', callback);
+	}
 });
 server.listen(465);
