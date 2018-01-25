33dea69538f89ee2fdcc3730ab7cb8e688a51bb3
7coil <leondrolio@gmail.com>
2018-01-25 00:11:16 +0000

    Inspect the mail object, disallow really big attatchments

--- server/mail.js
+++ server/mail.js
@@ -1,14 +1,23 @@
 const { SMTPServer } = require('smtp-server');
+const { simpleParser } = require('mailparser');
 const fs = require('fs');
 const config = require('config');
+const { inspect } = require('util');
 
 const server = new SMTPServer({
 	key: fs.readFileSync(config.get('certificate').key),
 	cert: fs.readFileSync(config.get('certificate').cert),
 	authOptional: true,
 	onData(stream, session, callback) {
-		stream.pipe(process.stdout);
-		stream.on('end', callback);
+		simpleParser(stream).then((mail) => {
+			console.dir(inspect(mail));
+			for (let i = 0; i < mail.attachments.length; i += 1) {
+				if (mail.attachments[i].size > 8000000) {
+					return callback(new Error('Attatchments must be less than 8MB in size'));
+				}
+			}
+			return callback();
+		});
 	}
 });
 server.listen(25);
