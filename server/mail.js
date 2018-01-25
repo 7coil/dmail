1705aed8ec63f0133de40979c9f27ef01ebbcdb2
7coil <leondrolio@gmail.com>
2018-01-25 00:46:57 +0000

    Bounces!

--- server/mail.js
+++ server/mail.js
@@ -1,8 +1,26 @@
 const { SMTPServer } = require('smtp-server');
 const { simpleParser } = require('mailparser');
+const nodemailer = require('nodemailer');
 const fs = require('fs');
 const config = require('config');
 const { inspect } = require('util');
+
+const transporter = nodemailer.createTransport({
+	sendmail: true,
+	newline: 'unix'
+});
+
+const bounce = (mail, error) => {
+	transporter.sendMail({
+		from: 'server@mss.ovh',
+		to: mail.from,
+		subject: 'Non-Delivery Report',
+		text: error
+	}, (err, info) => {
+		console.log(info.envelope);
+		console.log(info.messageId);
+	});
+};
 
 const server = new SMTPServer({
 	key: fs.readFileSync(config.get('certificate').key),
@@ -10,13 +28,16 @@ const server = new SMTPServer({
 	authOptional: true,
 	onData(stream, session, callback) {
 		simpleParser(stream).then((mail) => {
+			let error;
 			console.dir(inspect(mail, {
 				showHidden: false,
 				depth: null
 			}));
 			for (let i = 0; i < mail.attachments.length; i += 1) {
 				if (mail.attachments[i].size > 8000000) {
-					console.log('Attatchments must be less than 8MB in size');
+					error = 'Attatchments must be less than 8MB in size';
+					console.error(error);
+					bounce(mail, error);
 					return callback(new Error('Attatchments must be less than 8MB in size'));
 				}
 			}
@@ -25,3 +46,5 @@ const server = new SMTPServer({
 	}
 });
 server.listen(25);
+
+module.exports = transporter;
