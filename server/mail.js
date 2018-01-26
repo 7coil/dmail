9b5fd38571e65e796f82ca3d0cf28f46b5b5349c
7coil <leondrolio@gmail.com>
2018-01-26 14:06:18 +0000

    JSON stringify mail

--- server/mail.js
+++ server/mail.js
@@ -50,27 +50,23 @@ const server = new SMTPServer({
 	key: fs.readFileSync(config.get('mail.smtp-server.key')),
 	cert: fs.readFileSync(config.get('mail.smtp-server.cert')),
 	authOptional: true,
-	onData(stream, session, callback) {
-		simpleParser(stream).then((mail) => {
-			let error;
-			console.dir(inspect(mail, {
-				showHidden: false,
-				depth: null
-			}));
+	async onData(stream, session, callback) {
+		const mail = await simpleParser(stream);
+		let error;
+		console.dir(JSON.stringify(mail));
 
-			// Iterate through attatchments, and validate each one
-			for (let i = 0; i < mail.attachments.length; i += 1) {
-				// If an attachment is too large, throw it out of the window.
-				if (mail.attachments[i].size > 8000000) {
-					error = new Error('Your files are too powerful! Max file size 8.00Mb please.');
-					error.responseCode = 552;
-					return callback(error);
-				}
+		// Iterate through attatchments, and validate each one
+		for (let i = 0; i < mail.attachments.length; i += 1) {
+			// If an attachment is too large, throw it out of the window.
+			if (mail.attachments[i].size > 8000000) {
+				error = new Error('Your files are too powerful! Max file size 8.00Mb please.');
+				error.responseCode = 552;
+				return callback(error);
 			}
+		}
 
-			// Success!
-			return callback();
-		});
+		// Success!
+		return callback();
 	}
 });
 server.listen(25);
