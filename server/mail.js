b33f5c6f373f99236541c036c4d525ab0b02eb3b
7coil <leondrolio@gmail.com>
2018-01-26 14:19:32 +0000

    Remove bounce, log more

--- server/mail.js
+++ server/mail.js
@@ -3,48 +3,12 @@ const { simpleParser } = require('mailparser');
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 const config = require('config');
-const { inspect } = require('util');
 
 const transporter = nodemailer.createTransport(config.get('mail.nodemailer'));
 
 transporter.on('error', (err) => {
 	console.dir(err);
 });
-
-// const bounce = (mail, error) => {
-// 	if (mail.from && mail.from.value[0]) {
-// 		// References string, to reply to the original E-Mail
-// 		let references;
-
-// 		// Reply subject
-// 		let subject = 'Non-Delivery Report';
-
-// 		if (typeof mail.references === 'object' && Array.isArray(mail.references) && mail.references.length === 0) {
-// 			references = [mail.headers['Message-Id'], ...mail.references].join(' ');
-// 		} else {
-// 			references = mail.headers['Message-Id'];
-// 		}
-
-// 		if (mail.subject) {
-// 			subject += ' for: ';
-// 			subject += mail.subject;
-// 		}
-
-// 		transporter.sendMail({
-// 			from: 'noreply@mss.ovh',
-// 			to: mail.from.value[0].address,
-// 			subject,
-// 			text: error,
-// 			headers: {
-// 				'In-Reply-To': mail.headers['Message-Id'],
-// 				References: references
-// 			}
-// 		}, (err, info) => {
-// 			console.dir(info);
-// 			console.dir(err);
-// 		});
-// 	}
-// };
 
 const server = new SMTPServer({
 	key: fs.readFileSync(config.get('mail.smtp-server.key')),
@@ -54,6 +18,7 @@ const server = new SMTPServer({
 		const mail = await simpleParser(stream);
 		let error;
 		console.dir(JSON.stringify(mail));
+		console.dir(mail);
 
 		// Iterate through attatchments, and validate each one
 		for (let i = 0; i < mail.attachments.length; i += 1) {
